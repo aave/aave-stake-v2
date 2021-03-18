01a19189fcc3ed30382713caabb769a1800e5eba
sendra <polsendra@gmail.com>
2021-03-18 12:40:17 +0100

    fixed deployment

--- tasks/deployments/deploy-ClaimHelper.ts
+++ tasks/deployments/deploy-ClaimHelper.ts
@@ -21,16 +21,16 @@ task(`deploy-ClaimHelper`, `Deploys the ClaimStakingRewardsHelper contract`)
       [eEthereumNetwork.kovan]: {
         aaveStakeTokenAddress: '0xf2fbf9A6710AfDa1c4AaB2E922DE9D69E0C97fd2',
         bptStakeTokenAddress: '0x31ce45Ab6E26C72c47C52c27498D460099545ef2',
-        aaveToken: '0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9',
+        aaveToken: '0xb597cd8d3217ea6477232f9217fa70837ff667af',
       },
       [eEthereumNetwork.main]: {
         aaveStakeTokenAddress: '0x4da27a545c0c5b758a6ba100e3a049001de870f5',
         bptStakeTokenAddress: '0xa1116930326D21fB917d5A27F1E9943A9595fb47',
-        aaveToken: '0xb597cd8d3217ea6477232f9217fa70837ff667af',
+        aaveToken: '0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9',
       },
     };
 
-    console.log(`\tDeploying ClaimHelper implementation ...`);
+    console.log(`\tDeploying ClaimHelper ...`);
     const ClaimHelper = await deployClaimHelper(
       [
         stakeTokens[localBRE.network.name].aaveStakeTokenAddress,
